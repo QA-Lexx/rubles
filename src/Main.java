14595652ca5bd16e5fc47fe450be7740736ee0de
QA-Lexx <ilin_001@mail.ru>
2022-11-21 13:42:52 +0300

    Change code by adding conditional operator

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,12 @@ public class Main {
     public static void main(String[] args) {
 
         int initialAmount = 100;
-
         int addAmount = 1100;
 
-        if(addAmount <= 1000) {
+        if (addAmount <= 1000) {
             System.out.println((initialAmount + addAmount) + " Сумма на вашем счете");
             System.out.println((0) + " Из них бонусных рублей");
-        }
-
-        if(addAmount > 1000) {
+        } else {
             System.out.println(((initialAmount + addAmount) + (1 * addAmount / 100)) + " Сумма на вашем счете");
             System.out.println((1 * addAmount / 100) + " Из них бонусных рублей");
         }
